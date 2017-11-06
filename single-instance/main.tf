84fca496b512bc487cacd429d76c10a81e2e8e5c
Aaron Molitor <ammolitor@gmail.com>
2017-11-05 21:46:28 -0600

    add second disk, mount as document root and move index.html to heredoc in userdata.

--- single-instance/main.tf
+++ single-instance/main.tf
@@ -7,30 +7,40 @@ resource "aws_eip" "webserver" {
 }
 
 resource "aws_eip_association" "webserver" {
-  instance_id = "${aws_instance.webserver.id}"
+  instance_id   = "${aws_instance.webserver.id}"
   allocation_id = "${aws_eip.webserver.id}"
 }
 
 resource "aws_security_group" "webserver" {
   vpc_id = "vpc-5b3aa73d"
-  ingress {
-    from_port = 22
-    protocol  = "tcp"
-    to_port   = 22
-  }
+  name   = "webserver"
+}
 
-  ingress {
-    from_port = 80
-    protocol  = "tcp"
-    to_port   = 80
-  }
+resource "aws_security_group_rule" "ingress_http" {
+  from_port         = 80
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.webserver.id}"
+  to_port           = 80
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "ingress_ssh" {
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.webserver.id}"
+  to_port           = 22
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "egress_all" {
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = "${aws_security_group.webserver.id}"
+  to_port           = 65535
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 resource "aws_instance" "webserver" {
@@ -47,7 +57,20 @@ resource "aws_instance" "webserver" {
   associate_public_ip_address          = "false"
   private_ip                           = "172.31.32.4"
   source_dest_check                    = "true"
-  user_data                            = ""
+  user_data                            = "${file("user-data.sh")}"
+
+  root_block_device = {
+    volume_type           = "gp2"
+    volume_size           = "8"
+    delete_on_termination = "true"
+  }
+
+  ebs_block_device {
+    device_name           = "/dev/sdb"
+    volume_type           = "gp2"
+    volume_size           = "1"
+    delete_on_termination = "true"
+  }
 
   tags = {
     name       = "webserver"
@@ -61,11 +84,5 @@ resource "aws_instance" "webserver" {
     owner      = "Aaron Molitor"
     purpose    = "webserver"
     department = "testing"
-  }
-
-  root_block_device = {
-    volume_type           = "gp2"
-    volume_size           = "8"
-    delete_on_termination = "true"
   }
 }
--- single-instance/user-data.sh
+++ single-instance/user-data.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
+set -ex
+
+# backup fstab
+echo "backup fstab in /etc/fstab.DEFAULT"
+cat /etc/fstab > /etc/fstab.DEFAULT
+
+# setup partitions on data disks
+echo -e "o\nn\np\n1\n\n\nw" | fdisk /dev/xvdb
+
+# create XFS filesystem and add to fstab
+mkfs.xfs -L varwww -f /dev/xvdb1
+test -e /var/www || mkdir -p /var/www
+echo "LABEL=varwww /var/www xfs defaults,noatime 0 2" >> /etc/fstab
+sync
+mount /var/www
+
+yum -y install httpd
+systemctl start httpd
+
+# create index.html
+cat > /var/www/html/index.html << IDX
+<html>
+<p>Hello AWS World</p>
+</html>
+IDX
 
